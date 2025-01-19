b22aa5e8b7c73ac3171e138c0296551a1767218f
Aktishch <aktishch@gmail.com>
2025-01-19 17:21:50 +0300

    add 404 page

--- src/ts/smooth-scroll.ts
+++ src/ts/smooth-scroll.ts
@@ -1,28 +1,23 @@
 import { scrolledPage } from './utils'
 
 export default (): void => {
-  const wrapper = document.querySelector(
-    '*[data-smooth-wrapper]'
-  ) as HTMLElement
+  const wrappers = document.querySelectorAll('*[data-smooth-wrapper]') as NodeListOf<HTMLElement>
 
-  if (!wrapper) return
+  wrappers.forEach((wrapper: HTMLElement): void => {
+    if (!wrapper) return
 
-  const layers = wrapper.querySelectorAll(
-    '*[data-smooth-layer]'
-  ) as NodeListOf<HTMLElement>
-
-  layers.forEach((layer: HTMLElement): void => {
-    if (!layer) return
-
-    const layerSpeed: number = Number(layer.dataset.smoothSpeed) / 100 || 0.02
-    const layerDepth: number = Number(layer.dataset.smoothDepth) || 1
-    let layerPosition: number = 0
-
-    const createSmoothLayer = (): void => {
-      if (
-        wrapper.getBoundingClientRect().top - window.screen.height <= 0 &&
-        scrolledPage().top < wrapper.offsetTop + wrapper.offsetHeight
-      ) {
+    const layers = wrapper.querySelectorAll(
+      '*[data-smooth-layer]'
+    ) as NodeListOf<HTMLElement>
+  
+    layers.forEach((layer: HTMLElement): void => {
+      if (!layer) return
+  
+      const layerSpeed: number = Number(layer.dataset.smoothSpeed) / 100 || 0.02
+      const layerDepth: number = Number(layer.dataset.smoothDepth) || 1
+      let layerPosition: number = 0
+  
+      const createSmoothLayer = (): void => {
         layerPosition +=
           (scrolledPage().top - wrapper.offsetTop - layerPosition) * layerSpeed
 
@@ -50,11 +45,11 @@ export default (): void => {
           default:
             return
         }
+  
+        window.requestAnimationFrame(createSmoothLayer)
       }
-
+  
       window.requestAnimationFrame(createSmoothLayer)
-    }
-
-    window.requestAnimationFrame(createSmoothLayer)
+    })
   })
 }
